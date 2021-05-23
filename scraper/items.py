6fe243bcaf0a92bf9ce985baf5cd71e0842631d2
Jelmer <jelmer@plumerai.com>
2021-05-23 13:29:54 +0200

    Scraper outline and item definitions

--- scraper/items.py
+++ scraper/items.py
@@ -1,17 +1,49 @@
 from dataclasses import dataclass, field
-from typing import Dict
+from datetime import datetime
+from enum import Enum
+from typing import Optional, Sequence
+
+"""
+These just replicate the database specification. See the backend folder for details.
+"""
 
 
 @dataclass
-class StockItem:
-    """Represents a stock and its financial data."""
+class Motion:
+    motie_id: int
+    title: str
+    submit_date: datetime
+    vote_date: datetime
+    document_url: str
+    total_votes: int
+    submitter_id: int
+    first_signatory_id: Optional[int] = None
+    second_signatory_id: Optional[int] = None
+    co_submitter_id: Optional[int] = None
+    vote_ids: Sequence[int] = field(default_factory=lambda: [])
 
+
+@dataclass
+class Person:
+    first_name: str
+    last_name: str
+    party_id: int
+
+
+class Result(Enum):
+    For: 0
+    Against: 1
+
+
+@dataclass
+class Vote:
+    result: Result
+    votes: int
+    motion_id: int
+    party_id: int
+
+
+@dataclass
+class Party:
     name: str
-    symbol: str
-    description: str
-    sic: str  # Code and description of industry
-
-    # To be filled after initialization
-    income: Dict = field(default_factory=dict, init=False)
-    balance: Dict = field(default_factory=dict, init=False)
-    cash_flow: Dict = field(default_factory=dict, init=False)
+    code: str
