3a9a76249642cd1218b3ad7db930ff6e7fa1378e
Jelmer <jelmer@plumerai.com>
2021-06-20 17:58:46 +0200

    Find and crawl all motion URLs

--- scraper/spiders/stemmingsuitslagen.py
+++ scraper/spiders/stemmingsuitslagen.py
@@ -20,28 +20,39 @@ class StemmingsUitslagenSpider(scrapy.Spider):
             self.logger.warning(f"Did not know how to handle {response.url} !")
 
     def parse_homepage(self, response):
-        # TODO: select each individual card
-        table = []
+        # Parses the homepage for the batch motion result cards (one for each day)
+        cards = response.xpath(
+            "//div[@class='card' and normalize-space(descendant::div[@class='card__title']/text()) = 'Stemmingsuitslagen']"
+        )
+        # All cards are Stemmingsuitslagen, so we expect at least one per page.
+        assert len(cards) > 0
 
-        for row in table:
-            url = ""
+        # TODO: filter out the ones that have yet to take place
+
+        for card in cards:
+            url = card.xpath("div[@class='card__content']//div/h3/a/@href").get()
             yield Request(
                 response.urljoin(url),
-                callback=self.parse_sector_or_industry,
-                cb_kwargs={},
-                # TODO: this is probably only necessary if we need to load JS.
-                # args={
-                #     "wait": 1,
-                # },
+                callback=self.parse_motion_batch,
             )
 
         # TODO: move to next page
 
-    def parse_result_batch(self):
-        # TODO: select each individual motion card and parse it
-        pass
+    def parse_motion_batch(self, response):
+        # Retrieves the individual motions from a daily results page, e.g.
+        # https://www.tweedekamer.nl/kamerstukken/stemmingsuitslagen/detail?id=2021P10885
+        motion_urls = response.xpath("//a[@class='card']/@href").getall()
+        assert len(motion_urls) > 0
 
-    def parse_motion(self):
-        # TODO: Parse motion item including its results and yield all the relevant
-        # items to be inserted into the database.
-        pass
+        for url in motion_urls:
+            yield Request(
+                response.urljoin(url),
+                callback=self.parse_motion,
+            )
+
+    def parse_motion(self, response):
+        # Parses a specific motion and its results, e.g.
+        # https://www.tweedekamer.nl/kamerstukken/detail?id=2021Z11008&did=2021D23928
+        print(f"Parsing motion! {response.url}")
+
+        # TODO: scrape the actual info and call database endpoint
